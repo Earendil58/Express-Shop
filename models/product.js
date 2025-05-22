ef2fbfa4377e5ca39671dcc75b1821a6c7e9e7d1
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2025-05-22 15:17:59 -0300

    model from products

--- models/product.js
+++ models/product.js
@@ -1,4 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+const p = path.join(path(require.main.filename), 'data', 'products.json');
 
+
+module.exports = getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        }
+        else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+};
 
 class Product {
     constructor(title) {
@@ -6,6 +20,15 @@ class Product {
     }
 
     save() {
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err);
+            });
+        });
+    }
 
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
     }
 }
